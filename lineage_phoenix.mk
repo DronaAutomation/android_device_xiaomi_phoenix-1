32ede0aaff019cc8ab3c3c22a0f91994f84cc470
Drona <43744806+shivamjadon@users.noreply.github.com>
2021-02-08 13:39:33 +0530

    Update lineage_phoenix.mk

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -7,19 +7,19 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_phoenix
+PRODUCT_NAME := lineage_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
 
-# ArrowOS Properties
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Sreekanth
+# Sakura build flags
+SAKURA_OFFICIAL := true
+SAKURA_MAINTAINER := Shivam(Dronacharya)
+SAKURA_GAPPS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DISC="coral-user 11 RQ1A.210105.003 7005429 release-keys"
